242de4e78339aaec03be8afc2df585cc8005ab7c
Kristofer Göransson <kristofer@jojnts.com>
2016-11-15 17:06:38 +0100

    Clean up

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd ~/Projects/jojnts-service \;
 tmux new-session -s "dev" -n "sh" \; new-window -n "serve" \; split-window -h -b -c "angular" \; select-pane -t 0 \;
-#tmux attach-session -d
