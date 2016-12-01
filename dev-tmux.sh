051bb3097783349ed658dbd959b68f68fc81a692
Kristofer Göransson <kristofer@jojnts.com>
2016-12-01 10:48:54 +0100

    - Changed copy-mode binding

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ~/Projects/jojnts-service \;
-tmux new-session -s "dev" -n "sh" \; new-window -n "serve" \; split-window -h -b -c "angular" \; select-pane -t 0 \;
+tmux new-session -s "dev" -n "sh" \; new-window -n "serve" -c "angular" \; new-window -n "rails" \; select-pane -t 0 \;
